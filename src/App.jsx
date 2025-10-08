7d8e6b2f120f8b6802371ccf2bd8c5bf833407da
cloudybry <bryanbrzbl@gmail.com>
2025-10-08 22:18:29 +0800

    launch personal portfolio with custom React layout and futuristic design elements

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,12 @@ function App() {
 <header className="hero">
   <div className="hero-content">
     <div className="hero-image">
-      <img src="src\pictures\me.png" alt="Bryan Portrait" class="passport-photo" />
+      <img src="/me.png" alt="Bryan Portrait" class="passport-photo" />
     </div>
     <div className="hero-text">
       <h1 className="title">Bryan John Berzabal</h1>
       <p className="subtitle">Aspiring Junior Software Developer</p>
+      <p className="subtitle">Philippines</p>
       <a href="#projects" className="cta-button">Explore My Work</a>
     </div>
   </div>
@@ -30,31 +31,31 @@ function App() {
       <section id="projects" className="projects">
         <h2>Projects</h2>
       <div className="project-card">
-    <img src= "src\pictures\Terraform.png " alt="Terraform-localstock showcase" className="project-image" />
+    <img src= "/Terraform.png" alt="Terraform-localstock showcase" className="project-image" />
     <h3>Terraform-localstock showcase</h3>
     <a href="https://github.com/cloudybry/Terraform-localstack-showcase">View on GitHub</a>
   </div>
 
   <div className="project-card">
-    <img src=" src\pictures\JobTrack.png" alt="Job Track" className="project-image" />
+    <img src="/JobTrack.png" alt="Job Track" className="project-image" />
     <h3>Job Track</h3>
     <a href="https://github.com/cloudybry/JobTrack">View on GitHub</a>
   </div>
 
   <div className="project-card">
-    <img src=" src\pictures\MetricPulse.png" alt="Metric Pulse" className="project-image" />
+    <img src="/MetricPulse.png" alt="Metric Pulse" className="project-image" />
     <h3>Metric Pulse</h3>
     <a href="https://github.com/cloudybry/Metricpulse">View on GitHub</a>
   </div>
 
   <div className="project-card">
-    <img src="src\pictures\BookBase.png" alt="Book Base" className="project-image" />
+    <img src="/BookBase.png" alt="Book Base" className="project-image" />
     <h3>Book Base</h3>
     <a href="https://github.com/cloudybry/BookBase">View on GitHub</a>
   </div>
 
   <div className="project-card">
-    <img src="src\pictures\MindMesh.png" alt="Mind Mesh" className="project-image" />
+    <img src="/MindMesh.png" alt="Mind Mesh" className="project-image" />
     <h3>Mind Mesh</h3>
     <a href="https://github.com/cloudybry/Mindmesh">View on GitHub</a>
   </div>
