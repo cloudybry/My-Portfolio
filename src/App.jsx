166ca631faafb5b406c7ba5d87205f7ca20fe98d
cloudybry <bryanbrzbl@gmail.com>
2025-10-11 18:44:26 +0800

    Change Facebook to Linkedin for socials

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,8 @@ function Home() {
               <Link to="/projects" className="cta-button">Explore My Work &gt;</Link>
 
               <div className="social-buttons">
-                <a href="https://www.facebook.com/bryanberzbal" target="_blank" rel="noopener noreferrer" className="icon-button">
-                  <img src="/facebook.svg" alt="Facebook" className="social-icon" />
+                <a href="https://www.linkedin.com/in/bryanberzabal/" target="_blank" rel="noopener noreferrer" className="icon-button">
+                  <img src="/linkedin.svg" alt="Linkedin" className="social-icon" />
                 </a>
                 <a href="https://github.com/cloudybry" target="_blank" rel="noopener noreferrer" className="icon-button">
                   <img src="/github.svg" alt="GitHub" className="social-icon" />
